2010430b87556bf57d5518223e340e3613a2dcfe
Fer <ferp@protonmail.ch>
2024-07-25 16:08:07 +0200

    objects separated and rotating

--- src/script.js
+++ src/script.js
@@ -26,16 +26,33 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
+
+// Texture
+const textureLoader = new THREE.TextureLoader();
+const gradientTexture = textureLoader.load("/textures/gradients/5.jpg");
+gradientTexture.magFilter = THREE.NearestFilter;
+// Material
 const material = new THREE.MeshToonMaterial({
   color: parameters.materialColor,
+  gradientMap: gradientTexture,
 });
+// Meshes
+const objectsDistance = 4
 const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 32), material);
+
 const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 32), material);
 const mesh3 = new THREE.Mesh(
   new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
   material
 );
+
+mesh1.position.y = - objectsDistance * 0
+mesh2.position.y = - objectsDistance * 1
+mesh3.position.y = - objectsDistance * 2
+
 scene.add(mesh1, mesh2, mesh3);
+
+const sectionMeshes = [mesh1, mesh2, mesh3]
 
 /**
  * Lights
@@ -97,6 +114,12 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Animate meshes
+  for(const mesh of sectionMeshes) {
+    mesh.rotation.x = elapsedTime * 0.1
+    mesh.rotation.y = elapsedTime * 0.12
+  }
 
   // Render
   renderer.render(scene, camera);
